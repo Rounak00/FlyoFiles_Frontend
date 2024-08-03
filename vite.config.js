3f9c350901d8f5fe1a0513eb60a3e9d1b66480ed
Rounak00 <mukherjeerounak10@gmail.com>
2024-08-04 00:27:46 +0530

    FEAT : PWA Successfull

--- vite.config.js
+++ vite.config.js
@@ -3,61 +3,62 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 
-const manifestForPlugin={
-   resgisterTypes:"prompt",
-   includeAssets:['favicon.ico','apple-touch-icon.png','maskable-icon.png'],
-   manifest:{
+
+
+
+const manifestForPlugin = {
+   registerTypes: "prompt",
+   includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'maskable-icon.png'],
+   manifest: {
       name: "FlyoFiles",
       short_name: "FlyoFiles",
       description: "File Sharing App",
-      icons:[
+      icons: [
          {
-          src:"/logo192.png",
-          sizes:"192x192",
-          type: "image/png",
+            src: "/logo192.png",
+            sizes: "192x192",
+            type: "image/png",
          },
          {
-          src:"/logo512.png",
-          sizes:"512x512",
-          type: "image/png",
-          purpose:"favicon"
+            src: "/logo512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "favicon"
          },
          {
-          src:"/apple-touch-icon.png",
-          sizes:"180x180",
-          type: "image/png",
-          purpose:"apple touch icon"
+            src: "/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+            purpose: "apple touch icon"
          },
          {
-          src:"/logo144.png",
-          sizes:"144x144",
-          type: "image/png",
-          purpose:"any"
+            src: "/logo144.png",
+            sizes: "144x144",
+            type: "image/png",
+            purpose: "any"
          },
          {
-          src:"/logo256.png",
-          sizes:"256x256",
-          type: "image/png",
-          purpose:"icon"
+            src: "/logo256.png",
+            sizes: "256x256",
+            type: "image/png",
+            purpose: "icon"
          },
          {
-          src:"/logo384.png",
-          sizes:"384x384",
-          type: "image/png",
-          purpose:"any maskable"
-        },
-         {},
+            src: "/logo384.png",
+            sizes: "384x384",
+            type: "image/png",
+            purpose: "any maskable"
+         }
       ],
       theme_color: "#2196f3",
       background_color: "#2196f3",
       display: "standalone",
       scope: "/",
       start_url: "/"
-    
    }
 }
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),VitePWA(manifestForPlugin)],
+  plugins: [react(), VitePWA(manifestForPlugin)],
 })
