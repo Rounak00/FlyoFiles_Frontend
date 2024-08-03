0cd235ba4c70ab385442fc591cfd1c6ca2024d83
Rounak00 <mukherjeerounak10@gmail.com>
2024-08-03 17:16:04 +0530

    feat:Update component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useRef, useState, useEffect } from "react";
 import "./App.css";
-import { FaFileArrowUp } from "react-icons/fa6";
 import { uploadFile } from "./services/api";
-import { RiFileCopyFill } from "react-icons/ri";
-import { BarLoader } from "react-spinners";
+import ResultSection from "./components/ResultSection";
+import Loading from "./components/Loading";
+import HeadingLogo from "./components/HeadingLogo";
+import FileSection from "./components/FileSection";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Heading_Logo from './assets/Heading_Logo.svg'
 
 const notify = () => toast("File Uploaded Successfully");
 
@@ -50,13 +50,9 @@ function App() {
 
   return (
     <>
-      <div className="flex justify-center items-center">
-      <p className="h-32 w-64 ">
-        <img src={Heading_Logo} alt="Heading_Logo" className="h-full w-full"/>
-      </p>
-      </div>
+      <HeadingLogo/>
       <div className="flex justify-center items-center h-auto ">
-        <div className="w-[70%] h-auto bg-[#012e45] flex justify-start items-center flex-col gap-4 max-md:w-[100vw] px-6">
+        <div className="w-[70%] h-auto min-h-[30vh] bg-[#012e45] flex justify-start items-center flex-col gap-4 max-md:w-[100vw] ">
           <h1 className="pt-4 pb-4 font-bold text-[#b7cee0] text-center">
             FlyoFiles is a Simple File Sharing APP
             <br />
@@ -76,38 +72,11 @@ function App() {
               setFile(e.target.files[0]);
             }}
           />
-          {file && (
-            <div className="h-[20%] min-w-[20%] w-auto bg-blue-700 shadow-xl rounded-xl flex justify-evenly items-center gap-6 text-white ">
-              <div className="pl-4">
-                <FaFileArrowUp size={30} />
-              </div>
-              <div className="flex justify-center items-center pr-4 flex-col">
-                <p className="line-clamp-1">{file.name}</p>
-                <p>Size : {(file.size / 1024).toFixed(2)} mb</p>
-              </div>
-            </div>
-          )}
+          {file && (<FileSection file={file}/>)}
 
-          {isLoad && (
-            <div className="mt-4 flex justify-center items-center flex-col px-4">
-              {console.log("Spinner working")}
-              <BarLoader color="white" />
-              <p className="text-white mt-4">Please wait a While ...</p>
-            </div>
-          )}
-          {result && (
-            <div className="mt-4 flex flex-row gap-4 max-md:flex-col max-sm:text-sm max-sm:items-center px-4">
-              <span className="text-[#cce2f4] font-semibold">Your Link: </span>
-              <a href={result} target="_blank">
-                <span className="text-[#cce2f4] font-bold  max-sm:text-sm break-all">
-                  {result}
-                </span>
-              </a>
-              <span className="text-[#cce2f4] cursor-pointer " onClick={copyF}>
-                <RiFileCopyFill size={25} />
-              </span>
-            </div>
-          )}
+          {isLoad && ( <Loading/>)}
+
+          {result && ( <ResultSection result={result} copyF={copyF}/>)}
           <ToastContainer />
         </div>
       </div>
